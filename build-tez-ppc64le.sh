a02b3ea732332d7560303ad08a6de89a8495773f
pravin-dsilva <pravin_dsilva@persistent.com>
2016-08-08 12:32:22 +0530

    Update build-tez-ppc64le.sh

--- build-tez-ppc64le.sh
+++ build-tez-ppc64le.sh
@@ -1,5 +1,5 @@
-mkdir tmp; cd frontend-maven-plugin
 git clone https://github.com/OpenPOWER-BigData/frontend-maven-plugin.git
+cd frontend-maven-plugin
 mvn clean install -DskipTests
 cd ..
 rm -rf frontend-maven-plugin
